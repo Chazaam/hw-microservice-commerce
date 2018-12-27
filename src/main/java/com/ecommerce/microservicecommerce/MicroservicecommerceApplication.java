a8456b7e3862402cfc2b895a2518137101e63c0d
chazaam <samuel.lepajolec@gmail.com>
2018-12-27 13:44:36 +0100

    Init

---
+++ src/main/java/com/ecommerce/microservicecommerce/MicroservicecommerceApplication.java
@@ -0,0 +1,14 @@
+package com.ecommerce.microservicecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicecommerceApplication.class, args);
+	}
+
+}
+
